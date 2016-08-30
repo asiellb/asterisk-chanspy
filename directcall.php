2ac9622af18bc8ae50ae0db68a0224e4a1ba0bd0
Mironov Vladimir <vovax3m@ya.ru>
2016-08-30 23:12:28 +0300

    fix in directcall logic

--- directcall.php
+++ directcall.php
@@ -3,19 +3,30 @@ include 'func.php';
 require 'db_conn.php';
 $num = isset ( $_GET['num'] ) ? $_GET['num'] : '';
 $ext   = isset ( $_GET['ext'] )   ? $_GET['ext'] : '';
+$old   = isset ( $_GET['old'] )   ? $_GET['old'] : '';
+
 
 if ( $num  and  $ext ){
-$row = $cdrdb->query("SELECT id FROM directcalls WHERE num=${num}");
-    if ( mysqli_fetch_assoc($row) ) {
-	$query = "UPDATE directcalls SET `num` = ${num},`ext` = ${ext} WHERE num=${num}";
-    }
-    else {
-	$query = "INSERT IGNORE INTO `directcalls` SET `num` = ${num},`ext` = ${ext}";
-    }	
+	
+	$row = $cdrdb->query("SELECT id FROM directcalls WHERE num=${num}");
+	$row = mysqli_fetch_assoc($row);
+	
+    if( $old and $old !=$num ){
+		$query = "UPDATE IGNORE directcalls SET `num` = ${num},`ext` = ${ext} WHERE num = ${old} ";
+	}
+	else {
+		if ( $row  ) {
+			$query = "UPDATE directcalls SET `num` = ${num},`ext` = ${ext} WHERE num=${num}";
+		}
+		else {		
+			$query = "INSERT IGNORE INTO `directcalls` SET `num` = ${num},`ext` = ${ext}";
+		}
+	}
 }
 elseif ( $num and !$ext ){
-    $query="DELETE FROM `directcalls` WHERE `num` = ${num};";
+	$query="DELETE FROM `directcalls` WHERE `num` = ${num};";
 }
 if ( isset( $query )) $cdrdb->query( $query ) ;
 $cdrdb->close();
+
 ?>
